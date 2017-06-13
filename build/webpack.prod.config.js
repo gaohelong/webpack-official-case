833d26b4977eaedb3a3ae76182138bf19b2ce5a3
helong.gao <helong.gao@sinobbd.com>
2017-06-13 16:27:48 +0800

    webpack-office-case: webpack-dev-server

--- build/webpack.prod.config.js
+++ build/webpack.prod.config.js
@@ -7,14 +7,7 @@ module.exports = {
     /* output */
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    },
-
-    /* webpack-dev-server */
-    devServer: {
-        contentBase: path.join(__dirname, "dist"),
-        compress: true,
-        port: 8080
+        path: path.resolve(__dirname, '../dist')
     },
 
     /* 外部扩展(防止将某些 import 的包(package)打包到 bundle 中，而是在运行时(runtime)再去从外部获取这些扩展依赖(external dependencies)。) */
