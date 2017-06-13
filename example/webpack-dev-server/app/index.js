00590ff0d2b1214b50fe34c3340917405f71291c
helong.gao <helong.gao@sinobbd.com>
2017-06-13 15:13:54 +0800

    webpack-office-case：webpack-dev-server

---
+++ example/webpack-dev-server/app/index.js
@@ -0,0 +1,21 @@
+import _ from 'lodash';
+
+/* externals jquery */
+import $ from 'jquery';
+
+/* jquery */
+// npm install --save-dev jquery
+// import $ from 'jquery';
+
+function component () {
+    var element = document.createElement('div');
+
+    /* 需要引入 lodash，下一行才能正常工作 */
+    element.innerHTML = _.join(['Hello','webpack', $(window).height()], ' ');
+
+    $('body').css({'background-color': '#000', 'color': '#fff'});
+
+    return element;
+}
+
+document.body.appendChild(component());
