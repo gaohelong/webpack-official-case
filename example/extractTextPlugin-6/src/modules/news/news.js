1fc285502088caa4bb5fbf832c8b5f426672183f
helong.gao <helong.gao@sinobbd.com>
2017-06-15 13:14:27 +0800

    webpack-office-case: ExtractTextPlugin

---
+++ example/extractTextPlugin-6/src/modules/news/news.js
@@ -0,0 +1,25 @@
+/* sass */
+require('../../sass/main.scss');
+require('../../sass/modules/news/news.scss');
+
+import _ from 'lodash';
+
+/* externals jquery */
+import $ from 'jquery';
+
+/* jquery */
+// npm install --save-dev jquery
+// import $ from 'jquery';
+
+function component () {
+    var element = document.createElement('div');
+
+    /* 需要引入 lodash，下一行才能正常工作 */
+    element.innerHTML = _.join(['Hello','news', $(window).height()], ' ');
+
+    $('body').css({'background-color': '#fff', 'color': 'red'});
+
+    return element;
+}
+
+document.body.appendChild(component());
